d800cc0a4b31ec9e3425ecc9b4f2d1e7fad72ae6
Isha Kashif <ishak@udel.edu>
2025-08-06 08:58:09 -0400

    added minutes

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ my_time = int(input("Enter the time in seconds: "))
 
 for x in range(my_time, 0, -1):
     seconds = x % 60
-    print(f"00:00:{seconds:02}")
+    minutes = int(x / 60) % 60
+    print(f"00:00:{minutes:02}:{seconds:02}")
     time.sleep(1)
 
 print("TIME'S UP")
